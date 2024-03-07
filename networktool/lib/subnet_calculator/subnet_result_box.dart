dbb943effc83b0fecbae96d0ee2d585cb23c3f80
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-03-07 19:31:10 +0000

    added device count and wildcard calculator

--- networktool/lib/subnet_calculator/subnet_result_box.dart
+++ networktool/lib/subnet_calculator/subnet_result_box.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import '../globals.dart';
@@ -10,6 +12,22 @@ class SubnetResultBox extends StatelessWidget {
   final double octetThree;
   final double octetFour;
   final double mask;
+
+  String calculateSubnetMask(int mask) {
+    List<int> subnetMaskOctets = [];
+      
+    for (int i = 0; i < 4; i++) {
+      int onesInOctet = mask >= 8 ? 8 : mask;
+      
+      int octetValue = (1 << (8 - onesInOctet)) - 1;
+      subnetMaskOctets.add(octetValue);
+
+      mask -= onesInOctet;
+    }
+      
+    return subnetMaskOctets.join('.');
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +47,28 @@ class SubnetResultBox extends StatelessWidget {
               padding: const EdgeInsets.only(left: 8, top: 8),
               child: Text(
                 "subnet: $octetOne.$octetTwo.$octetThree.$octetFour/$mask",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 8),
+              child: Text(
+                mask < 31 ? "devices: ${pow(2, 32 - mask)}(-2)" : "devices: ${pow(2, 32 - mask)}",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8, top: 8),
+              child: Text(
+                "wildcard: ${calculateSubnetMask(mask.toInt())}",
                 style: const TextStyle(
                   color: Colors.black,
                   fontSize: 12,
